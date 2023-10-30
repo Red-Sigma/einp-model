32035ef259fda62b0804c8a886cdd2e95604e92f
Sergio Jimenez <sergio.jimenezcedron@icloud.com>
2023-10-30 16:51:09 +0100

    Update perimeter layer to support vector data

--- GeoRasterBlueprint/Model/Perimeter.cs
+++ GeoRasterBlueprint/Model/Perimeter.cs
@@ -4,22 +4,17 @@ using Mars.Interfaces.Environments;
 namespace GeoRasterBlueprint.Model;
 
 /// <summary>
-///     A raster layer that ingests an .asc raster file with the walkable area by the agents.
+///     A vector layer that ingests a .geojson vector file with the walkable area by the agents.
 /// </summary>
-public class Perimeter : RasterLayer
-{
+public class Perimeter : VectorLayer {
     /// <summary>
     ///     Checks for the coordinate whether this point is inside the perimeter (the defined polygon).
     /// </summary>
     /// <param name="coordinate">The coordinate to check</param>
     /// <returns>
-    ///     Returns true if the coordinate is inside the perimeter
+    ///     Returns true if the coordinate is inside the perimeter.
     /// </returns>
-    public bool IsPointInside(Position coordinate)
-    {
-        // First, check if the coordinate is inside the area of the .asc file.
-        // The comparison with "1" ensures that it's not a "non-walkable" cell (the area outside of our polygon).
-        // It's the NoData value that was set previously in QGIS.
-        return Extent.Contains(coordinate.X, coordinate.Y) && GetValue(coordinate) != 1;
+    public bool IsPointInside(Position coordinate) {
+        return Extent.Contains(coordinate.X, coordinate.Y);
     }
 }
